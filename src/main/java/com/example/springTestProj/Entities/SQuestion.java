900dff4234c95c327aa3701e3461ab8a73e3b871
gingerails <rocketcityabi@gmail.com>
2022-11-08 11:45:07 -0600

    updated attribute names to match column names in our db. DB should also be updated - will check for this after commit

---
+++ src/main/java/com/example/springTestProj/Entities/SQuestion.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.springTestProj.Entities;
+import lombok.*;
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+@Table(name = "SQuestion")
+public class SQuestion {
+    @Id
+   // @GeneratedValue(strategy=GenerationType.IDENTITY)  idek
+    @Column(name = "question_id")
+    private String questionID;
+    @Column(name = "question_content")
+    private String qContent;
+    @Column(name = "question_graphic")
+    private String qGraphic;
+    @Column(name = "correct_answer")
+    private String correctAnswer;
+    @Column(name = "answer_graphic")
+    private String answerGraphic;
+    @Column(name = "text_ref_section")
+    private String textReferenceSection;
+    @Column(name = "reference_material")
+    private String referenceMaterial;
+    @Column(name = "instructor_comment")
+    private String instructorComment;
+    @Column(name = "grading_instruction")
+    private String gradingInstruction;
+    @Column(name = "class_average")
+    private Double classAverage;
+    
+
+    public SQuestion() {
+    }
+
+    public SQuestion(String questionID) {
+        this.questionID = questionID;
+    }
+
+    @Override
+    public String toString() {
+        return "SQuestion [question_id=" + questionID + "]";
+    }
+
+}
