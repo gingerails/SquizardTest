db0bacb916652b507a2f07f20266f8c954168d4c
Orames <Orames@LAPTOP-ET80NS8S>
2022-11-09 22:00:04 -0600

    dded data connection to createTest.fmxl

--- src/main/java/com/example/springTestProj/Controller/CreateTestController.java
+++ src/main/java/com/example/springTestProj/Controller/CreateTestController.java
@@ -5,18 +5,23 @@
  */
 package com.example.springTestProj.Controller;
 
+import com.example.springTestProj.Entities.Courses;
+import com.example.springTestProj.Service.CourseService;
 import com.example.springTestProj.Service.UserService;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import net.rgielen.fxweaver.core.FxControllerAndView;
 import net.rgielen.fxweaver.core.FxWeaver;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
-
+import com.example.springTestProj.Service.CourseService;
+import java.util.ArrayList;
+import java.util.List;
 /**
  * FXML Controller class
  *
@@ -27,23 +32,29 @@ import org.springframework.stereotype.Component;
 public class CreateTestController implements ControlSwitchScreen {
     private final UserService userService;
     private final FxWeaver fxWeaver;
+    private final CourseService courseService;
     private Stage stage;
 
     @FXML
     private VBox mainVbox;
     @FXML
     private Button createTest;
-    
+    @FXML
+    private ComboBox section;
+    @FXML
+    private ComboBox classes;
    
-    public CreateTestController(UserService userService, FxWeaver fxWeaver) {
+    public CreateTestController(UserService userService, FxWeaver fxWeaver,CourseService courseService) {
         System.out.println("Create Test Controller");
         this.fxWeaver = fxWeaver;
         this.userService = userService;
+        this.courseService=courseService;
     }
 
     @FXML
     public void initialize () {
-
+        getCoursesInfo();
+        getSectionInfo();
         this.createTest.setOnAction(actionEvent -> {
             System.out.print("create button pressed");
             loadTestMaker();
@@ -73,4 +84,25 @@ public class CreateTestController implements ControlSwitchScreen {
                 = fxWeaver.load(TestMakerController.class);
         testMakerControllerAndView.getController().show(getCurrentStage());
     }
+     public void getCoursesInfo()
+    {
+       classes.getItems().clear();
+      List<Courses> dropdownCourseList = courseService.readCourses();
+        for (Courses course : dropdownCourseList) {
+
+                String statementString = course.getCoursesPrimaryKey().getCourseNum();
+                System.out.println(statementString);
+                
+                classes.getItems().addAll(statementString);
+        }
+    }
+     //needs to be completed needs to take data of what is in the class combobox and look for any section numbers
+       public void getSectionInfo()
+    {
+       
+                
+                section.getItems().addAll("ALL");
+    }
 }
+
+
