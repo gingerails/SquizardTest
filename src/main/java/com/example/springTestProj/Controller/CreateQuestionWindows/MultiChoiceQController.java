9bf549c3af0f1203d392a7cfe1434264cd19fb6d
gingerails <rocketcityabi@gmail.com>
2022-11-29 01:33:02 -0600

    multiple choice error checking bug fix Next:
    - view testkey, within maincontroller and TestMakerController
    - filter returned tests/qs by user id
    - maybe search tests by name using search bar
    - delete questions during test creation
    - reordering functionality

--- src/main/java/com/example/springTestProj/Controller/CreateQuestionWindows/MultiChoiceQController.java
+++ src/main/java/com/example/springTestProj/Controller/CreateQuestionWindows/MultiChoiceQController.java
@@ -2,6 +2,7 @@ package com.example.springTestProj.Controller.CreateQuestionWindows;
 
 import static com.example.springTestProj.Controller.CreateQuestionWindows.MatchingQController.path;
 import static com.example.springTestProj.Controller.CreateQuestionWindows.MatchingQController.pathTo;
+
 import com.example.springTestProj.Controller.QuestionHTMLHelper;
 import com.example.springTestProj.Controller.TestMakerController;
 import com.example.springTestProj.Entities.QuestionEntities.MultiChoiceQuestion;
@@ -9,8 +10,10 @@ import com.example.springTestProj.Entities.Test;
 import com.example.springTestProj.Service.QuestionService.MultiChoiceQService;
 import com.example.springTestProj.Service.TestService;
 import com.example.springTestProj.Service.UserService;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
+
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -68,9 +71,9 @@ public class MultiChoiceQController implements ControlDialogBoxes {
     @FXML
     private Label error;
 
-     public static String path = "src\\main\\resources\\";
+    public static String path = "src\\main\\resources\\";
     public static String pathTo = "";
-    
+
     public MultiChoiceQController(UserService userService, FxWeaver fxWeaver, TestService testService, MultiChoiceQService multiChoiceQService, TestMakerController testMakerController, QuestionHTMLHelper questionHTMLHelper) {
         this.testService = testService;
         this.fxWeaver = fxWeaver;
@@ -81,49 +84,42 @@ public class MultiChoiceQController implements ControlDialogBoxes {
     }
 
     @FXML
-    public void initialize () {
-        
-         String cSection="";
-        String cClass="";
-        int count =0;
+    public void initialize() {
+
+        String cSection = "";
+        String cClass = "";
+        int count = 0;
         //need to check current section and class
         BufferedReader reader;
-		try {
-			reader = new BufferedReader(new FileReader(
-					"temp.txt"));
-			String line = reader.readLine();
-			while (line != null) {
-                            
-				System.out.println(line);
-				// read next line
-                                if(count==0)
-                                {
-                                    cClass=line;
-                                }
-                                if(count==1)
-                                {
-                                    cSection=line;
-                                }
-				line = reader.readLine();
-                                count++;
-			}
-			reader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	
-                
-                //Files.deleteIfExists(Paths.get("temp.txt"));
-        System.out.println(cClass+" "+cSection);
-        
-        pathTo = path+cClass+"\\" +cSection+"\\";
-        
-        
-        
-        
-        
-        
-        
+        try {
+            reader = new BufferedReader(new FileReader(
+                    "temp.txt"));
+            String line = reader.readLine();
+            while (line != null) {
+
+                System.out.println(line);
+                // read next line
+                if (count == 0) {
+                    cClass = line;
+                }
+                if (count == 1) {
+                    cSection = line;
+                }
+                line = reader.readLine();
+                count++;
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        //Files.deleteIfExists(Paths.get("temp.txt"));
+        System.out.println(cClass + " " + cSection);
+
+        pathTo = path + cClass + "\\" + cSection + "\\";
+
+
         this.stage = new Stage();
         stage.setTitle("Add Multiple Choice Question");
         stage.setScene(new Scene(mcQuestionBox));
@@ -136,12 +132,12 @@ public class MultiChoiceQController implements ControlDialogBoxes {
                 throw new RuntimeException(e);
             }
         });
-        
+
         this.addAnswerGraphic.setOnAction(actionEvent -> {
             System.out.print("Add graphic button pushed");
-            
+
         });
-                
+
     }
 
 
@@ -152,55 +148,52 @@ public class MultiChoiceQController implements ControlDialogBoxes {
     }
 
 
-
     public void createQuestion() throws IOException {
         // gets the current stage, sets the scene w the create account control/view (fxweaver), then updates stage w that scene
         System.out.println("Add PRSSEDDDD");
-       if (choice1Field.getText().isBlank() ||
-               choice2Field.getText().isBlank() ||
-               choice3Field.getText().isBlank() ||
-               choice4Field.getText().isBlank() ||
-               answerTextField.getText().isBlank() ||
-               questionContent.getText().isBlank()){
-           System.out.println("SOMETHING WAS LEFT BLANK");
-           error.setText("Error: Must fill out each choice, question and answer!");
-       }
-       if(choice1Field.getText().equals(answerTextField.getText())==false&&choice2Field.getText().equals(answerTextField.getText())==false&&choice3Field.getText().equals(answerTextField.getText())==false&&choice4Field.getText().equals(answerTextField.getText())==false)
-       {
-           error.setText("Error: Answer is not a choice");
-       }
-        else{
-           String question = questionContent.getText();
-           String correctAnswer = answerTextField.getText();
-           String[] falseAnswers = {choice1Field.getText(),choice2Field.getText(),choice3Field.getText(),choice4Field.getText()};
-           String falseAnswer = Arrays.toString(falseAnswers);
+        if (choice1Field.getText().isBlank() ||
+                choice2Field.getText().isBlank() ||
+                choice3Field.getText().isBlank() ||
+                choice4Field.getText().isBlank() ||
+                answerTextField.getText().isBlank() ||
+                questionContent.getText().isBlank()) {
+            System.out.println("SOMETHING WAS LEFT BLANK");
+            error.setText("Error: Must fill out each choice, question and answer!");
+        }
+        else if (choice1Field.getText().equals(answerTextField.getText()) == false && choice2Field.getText().equals(answerTextField.getText()) == false && choice3Field.getText().equals(answerTextField.getText()) == false && choice4Field.getText().equals(answerTextField.getText()) == false) {
+            error.setText("Error: Answer is not a choice");
+        } else {
+            String question = questionContent.getText();
+            String correctAnswer = answerTextField.getText();
+            String[] falseAnswers = {choice1Field.getText(), choice2Field.getText(), choice3Field.getText(), choice4Field.getText()};
+            String falseAnswer = Arrays.toString(falseAnswers);
 
-           MultiChoiceQuestion multiChoiceQuestion = multiChoiceQService.createMCQuestion(question, correctAnswer, falseAnswer);
-           checkFieldsAndAddQuestion(multiChoiceQuestion);
-           Test currentTest = getCurrentTestSectionInfo();
-           String testFile = currentTest.getTestName();
-           addHTML(pathTo + testFile, pathTo + "KEY_" + testFile);
-           testMakerController.refresh();
-           stage.close();
-       }
+            MultiChoiceQuestion multiChoiceQuestion = multiChoiceQService.createMCQuestion(question, correctAnswer, falseAnswer);
+            checkFieldsAndAddQuestion(multiChoiceQuestion);
+            Test currentTest = getCurrentTestSectionInfo();
+            String testFile = currentTest.getTestName();
+            addHTML(pathTo + testFile, pathTo + "KEY_" + testFile);
+            testMakerController.refresh();
+            stage.close();
+        }
     }
-    
-    public void checkFieldsAndAddQuestion(MultiChoiceQuestion multiChoiceQuestion){
 
-        if(!referenceMaterial.getText().isBlank()){
-            String refMaterial =  referenceMaterial.getText();
+    public void checkFieldsAndAddQuestion(MultiChoiceQuestion multiChoiceQuestion) {
+
+        if (!referenceMaterial.getText().isBlank()) {
+            String refMaterial = referenceMaterial.getText();
             multiChoiceQuestion.setReferenceMaterial(refMaterial);
         }
-        if(!referenceSection.getText().isBlank()){
-            String refSection =  referenceSection.getText();
+        if (!referenceSection.getText().isBlank()) {
+            String refSection = referenceSection.getText();
             multiChoiceQuestion.setTextReferenceSection(refSection);
         }
-        if(!instructorComment.getText().isBlank()){
-            String comment =  instructorComment.getText();
+        if (!instructorComment.getText().isBlank()) {
+            String comment = instructorComment.getText();
             multiChoiceQuestion.setInstructorComment(comment);
         }
-        if(!gradingInstructions.getText().isBlank()){
-            String instructions =  gradingInstructions.getText();
+        if (!gradingInstructions.getText().isBlank()) {
+            String instructions = gradingInstructions.getText();
             multiChoiceQuestion.setGradingInstruction(instructions);
         }
         multiChoiceQuestion.setCreatorId(userService.returnCurrentUserID());
@@ -212,14 +205,16 @@ public class MultiChoiceQController implements ControlDialogBoxes {
 
     /**
      * Get currentTest obj from TestService, which contains a copy of the test being edited
+     *
      * @return
      */
-    public Test getCurrentTestSectionInfo(){
+    public Test getCurrentTestSectionInfo() {
         Test currentTest = testService.returnThisTest();
         System.out.println(currentTest.getTestUUID());
 
         return currentTest;
     }
+
     public void addHTML(String file, String keyFile) throws IOException {
         questionHTMLHelper.updateSections(file, keyFile);
     }
